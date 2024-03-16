778cf53d50bde66b89a2a3e2d1f72bf85c5df9ae
Grubysamuraj <jakub.dragosz@o2.pl>
2024-03-16 13:21:46 +0100

    home dodane

---
+++ app/(protected)/home.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Text } from "react-native-paper";
+
+export default function homeComponent() {
+  return (
+    <>
+      <Text variant="displayLarge">dziala?</Text>
+    </>
+  );
+}
