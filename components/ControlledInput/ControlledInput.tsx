6eeb2fe18033fca9e357419b5376e3be3097ebee
Inevera11 <zuzannabozek2002@gmail.com>
2024-03-16 13:08:25 +0100

    login changes

--- components/ControlledInput/ControlledInput.tsx
+++ components/ControlledInput/ControlledInput.tsx
@@ -1,43 +1,24 @@
-import { FieldValues, useController } from "react-hook-form";
-import { ControlledInputProps } from "./types";
-import React, { ReactElement } from "react";
-import { TextInput, View, Text } from "react-native";
+import { FieldValues, useController } from 'react-hook-form';
+import { ControlledInputProps } from './types';
+import React, { ReactElement } from 'react';
+import { View } from 'react-native';
+import { TextInput, Text } from 'react-native-paper';
 
-const ControlledInput = <T extends FieldValues>({
-  name,
-  label,
-  control,
-  secure,
-  placeholder = "Type...",
-}: ControlledInputProps<T>): ReactElement => {
-  const {
-    field: { value, onChange },
-    fieldState: { error },
-  } = useController({ control, name });
+const ControlledInput = <T extends FieldValues>({ name, label, control, secure, placeholder = 'Type...' }: ControlledInputProps<T>): ReactElement => {
+    const {
+        field: { value, onChange },
+        fieldState: { error },
+    } = useController({ control, name });
 
-  return (
-    <View style={{ width: "100%" }}>
-      <Text style={{ marginLeft: 16, color: "#10663F", fontWeight: "bold" }}>
-        {label}
-      </Text>
-      <TextInput
-        style={{
-          borderRadius: 16,
-          paddingHorizontal: 8,
-          paddingVertical: 8,
-          backgroundColor: "#E8E8E8",
-          elevation: 4,
-        }}
-        placeholder={placeholder}
-        secureTextEntry={secure}
-        value={value}
-        onChangeText={onChange}
-      />
-      {error?.message ? (
-        <Text style={{ marginLeft: 16, color: "red" }}>{error.message}</Text>
-      ) : null}
-    </View>
-  );
+    return (
+        <View style={{ width: '100%' }}>
+            <Text variant="headlineSmall" style={{ marginLeft: 16, marginBottom: 8 }}>
+                {label}
+            </Text>
+            <TextInput mode="outlined" placeholder={placeholder} secureTextEntry={secure} value={value} onChangeText={onChange} />
+            {error?.message ? <Text style={{ marginLeft: 16, color: 'red' }}>{error.message}</Text> : null}
+        </View>
+    );
 };
 
 export default ControlledInput;
